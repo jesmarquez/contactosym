5974789faff02243843dd0644116d59d3b42fa77
Jesus Marquez <jesmqz@gmail.com>
2021-07-23 23:48:22 -0400

    Save contacto

--- src/Controller/ContactoController.php
+++ src/Controller/ContactoController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contacto;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use \DateTime;
 
 class ContactoController extends AbstractController
 {
@@ -13,5 +17,36 @@ class ContactoController extends AbstractController
         return $this->render('contacto.html.twig', [
             'number' => $number,
         ]);
+    }
+
+    public function createProduct(ValidatorInterface $validator): Response
+    {
+        date_default_timezone_set('America/Bogota');
+        
+        $hoy = new \DateTime();
+        $entityManager = $this->getDoctrine()->getManager();
+  
+        $contacto = new Contacto();
+        $contacto->setNombre('Claris');
+        $contacto->setApellido('Marquez');
+        $contacto->setCelular('(766)-898-99-98');
+        $contacto->setCorreo('david@gmail.com');
+        $contacto->setArea('ventas');
+        $contacto->setCreado($hoy);
+        $contacto->setActualizado($hoy);
+        
+        $entityManager->persist($contacto);
+        $entityManager->flush();
+
+        $errors = $validator->validate($contacto);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
+        
+
+
+
+
+        return new Response('Saved new contact with id '.$contacto->getId());
     }
 }
---
+++ src/Entity/Contacto.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactoRepository::class)
+ */
+class Contacto
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $apellido;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $correo;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $celular;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $area;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creado;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $actualizado;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellido(): ?string
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido(string $apellido): self
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    public function getCorreo(): ?string
+    {
+        return $this->correo;
+    }
+
+    public function setCorreo(string $correo): self
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+
+    public function getCelular(): ?string
+    {
+        return $this->celular;
+    }
+
+    public function setCelular(string $celular): self
+    {
+        $this->celular = $celular;
+
+        return $this;
+    }
+
+    public function getArea(): ?string
+    {
+        return $this->area;
+    }
+
+    public function setArea(string $area): self
+    {
+        $this->area = $area;
+
+        return $this;
+    }
+
+    public function getCreado(): ?\DateTimeInterface
+    {
+        return $this->creado;
+    }
+
+    public function setCreado(\DateTimeInterface $creado): self
+    {
+        $this->creado = $creado;
+
+        return $this;
+    }
+
+    public function getActualizado(): ?\DateTimeInterface
+    {
+        return $this->actualizado;
+    }
+
+    public function setActualizado(\DateTimeInterface $actualizado): self
+    {
+        $this->actualizado = $actualizado;
+
+        return $this;
+    }
+}
